a8fb450fcceb76aa8c99728841265af459410061
李健夫 <hzlijianfu@corp.netease.com>
2020-02-10 17:25:26 +0800

    fish doc更新依赖

---
+++ .temp/pages/button/index.js
@@ -0,0 +1,29 @@
+import Nerv from "nervjs";
+import Taro from "@tarojs/taro-h5";
+import { View, Text } from '@tarojs/components';
+import './index.less';
+export default class Button extends Taro.Component {
+  constructor() {
+    super(...arguments);
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+  }
+  componentWillMount() {}
+  componentDidMount() {}
+  componentWillUnmount() {}
+  componentDidShow() {}
+  componentDidHide() {}
+  render() {
+    return <View className="index">
+        <Text>Button</Text>
+      </View>;
+  }
+  config = {
+    navigationBarTitleText: '首页'
+  };
+}
---
+++ site/doc.mobile.config.js
@@ -0,0 +1,42 @@
+/**
+ * 定制webpack相关配置
+ */
+const path = require('path')
+
+module.exports = {
+  // 启动端口
+  port: 4100,
+  // 入口名称，默认值`index`
+  entryName: 'site',
+  // 主题模版位置
+  theme: './site/mobile',
+  // html模版路径
+  htmlTemplate: path.join(__dirname, './mobile/template.html'),
+  // html输出名称，默认值`index`
+  htmlFileName: 'index',
+  // 文件输出路径
+  outputPath: './dist/mobile',
+  // 同webpack publicPath
+  publicPath: './',
+  // css中引用的资源路径，如果publicPath配置了相对目录，请配置 ../ 使引用目录正确，否则会出现资源引用路径异常，若未配置，则默认使用pulbicPath
+  MiniCssExtractPluginPublicPath: '../',
+  // dll配置
+  dll: {
+    name: 'mobileDll',
+    value: [
+      'react',
+      'react-dom',
+      'react-router',
+      'marked',
+      'less'
+    ]
+  },
+  // 自定义webpack配置
+  webpackConfig (config) {
+    config.resolve.alias = Object.assign({}, config.resolve.alias, {
+      '@docs': path.join(__dirname, './docs'),
+      '@': path.join(__dirname, './mobile')
+    })
+    return config
+  }
+}
